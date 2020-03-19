e29eab45aa6c3efe5bd24a51591760c05753c2e0
Anthony Ikeda <anthony.ikeda@gmail.com>
2020-03-19 09:06:04 -0700

    External log method now working again. Bumped to 0.1.6

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
 extern crate wasm_bindgen;
 
 use wasm_bindgen::prelude::*;
-
 #[wasm_bindgen]
 extern "C" {
-    #[wasm_bindgen]
+    #[wasm_bindgen(js_namespace = console)]
     fn log(s: &str);
-}
+ }
 
 #[wasm_bindgen]
-pub fn process_message(msg: &str) {
-    log(&format!("Welcome {}", msg));
+pub fn process_message(msg: &str) -> String {
+    log("Printing message");
+    format!("Welcome {}", &msg)
 }
